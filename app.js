eb28d28e6af31f3e9e889b12aeedbb84b0fd1a5c
Nelton Fernandes <47947664+neltonf@users.noreply.github.com>
2021-08-10 11:13:09 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+let dec = document.getElementById("dec");
+let res = document.getElementById("res");
+let inc = document.getElementById("inc");
+var num = document.getElementById("counterVal").innerHTML;
+
+inc.addEventListener("click", function () {
+  num = num + 1;
+  document.getElementById("counterVal").innerHTML = num;
+});
+
+res.addEventListener("click", function () {
+  num = 0;
+  document.getElementById("counterVal").innerHTML = num;
+});
+
+dec.addEventListener("click", function () {
+  num = num - 1;
+  document.getElementById("counterVal").innerHTML = num;
+});
