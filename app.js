e4799f321d0cb9cf69ba3e064ffdec0f64c7bad8
Nelton Fernandes <47947664+neltonf@users.noreply.github.com>
2021-09-07 18:01:21 +0530

    FIXED: num error

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let dec = document.getElementById("dec");
 let res = document.getElementById("res");
 let inc = document.getElementById("inc");
-var num = document.getElementById("counterVal").innerHTML;
+var num = Number(document.getElementById("counterVal").innerHTML);
 
 inc.addEventListener("click", function () {
   num = num + 1;
